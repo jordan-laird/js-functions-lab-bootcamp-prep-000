6f1e34f0205b8f225a1e0e4c5b6ef6e56f323109
stellar-lambda-0453 <jordanmack.laird@gmail.com>
2018-09-24 00:25:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays(string){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo(string){
+function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
